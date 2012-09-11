18ddae3b1e0a8b1fbc54791719b592967871b5d8
Sarjono Mukti Aji <me@simukti.net>
2012-09-11 20:23:11 +0700

    fix __DIR__ to PHP 5.2.* compliance

--- src/init/autoload.php
+++ src/init/autoload.php
@@ -1,6 +1,6 @@
 <?php
 return call_user_func(function() {
-    $includePaths = include __DIR__ . DS . 'include_paths.php';
+    $includePaths = include dirname(__FILE__) . DS . 'include_paths.php';
     set_include_path(implode(PATH_SEPARATOR, $includePaths));
     
     require_once (LIB_PATH . DS. 'zf1'. DS . 'Zend' . DS . 'Loader' . DS .'Autoloader.php');
